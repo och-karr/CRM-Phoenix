ba97f0d5aa0433fb3d0d6c23327def58dd74fce7
och-karr <okaa93@gmail.com>
2023-03-10 10:27:45 +0100

    feat: add assets - css, images, js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LoginComponentModule } from './components/login/login.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{ path: 'auth/login', component: LoginComponent }]), LoginComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
