7c04118fc1be0ea4ce5335adfc95d128fb8f2b15
och-karr <okaa93@gmail.com>
2023-03-11 13:05:42 +0100

    feat: add login component

---
+++ src/app/components/login/login.component-module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './login.component';
+import {RouterLink, RouterModule} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+
+@NgModule({
+  imports: [
+    RouterLink,
+    RouterModule,
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: [LoginComponent],
+  providers: [],
+  exports: [LoginComponent]
+})
+export class LoginComponentModule {
+}
