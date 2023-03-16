ad0d0bd78f3c5d956614fdf3e730aae5e713d827
och-karr <okaa93@gmail.com>
2023-03-16 11:13:52 +0100

    feat: display leads values

--- src/app/components/leads-table/leads-table.component.ts
+++ src/app/components/leads-table/leads-table.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {UserService} from "../../services/user.service";
-import {Router} from "@angular/router";
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { LeadsService } from '../../services/leads.service';
 
 @Component({
   selector: 'app-leads-table',
@@ -10,7 +12,9 @@ import {Router} from "@angular/router";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LeadsTableComponent {
-  constructor(private _userService: UserService, private _router: Router) {}
+  readonly leads$: Observable<any> = this._leadsService.getLeads();
+
+  constructor(private _userService: UserService, private _router: Router, private _leadsService: LeadsService) { }
 
   logoutUser() {
     this._userService.logout();
